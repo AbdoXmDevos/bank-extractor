c4ba57e31536a230c2dc32f18ca580e060662653
AbdoXmDevos <abdssamadkarimi.ak@gmail.com>
2025-08-02 19:08:28 +0100

    first commit v3.1

--- src/components/dashboard/CategoryCharts.tsx
+++ src/components/dashboard/CategoryCharts.tsx
@@ -141,7 +141,6 @@ export default function CategoryCharts({ operations }: CategoryChartsProps) {
           <ResponsiveContainer width="100%" height={400}>
             <RadialBarChart cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" data={currentData}>
               <RadialBar
-                minAngle={15}
                 label={{ position: 'insideStart', fill: '#fff' }}
                 background
                 clockWise
