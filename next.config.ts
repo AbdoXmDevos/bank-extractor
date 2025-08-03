63d4e21303085f7639f1ba0cbfeefac2ade88da3
AbdoXmDevos <abdssamadkarimi.ak@gmail.com>
2025-08-03 23:38:12 +0100

    fix enoent problem v1

--- next.config.ts
+++ next.config.ts
@@ -10,17 +10,7 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: false,
   },
 
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Redirect test file access to public directory
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        './test/data/05-versions-space.pdf': require.resolve('./public/test-data/05-versions-space.pdf'),
-      };
-    }
 
-    return config;
-  },
 };
 
 export default nextConfig;
